dfc7136089977fa6f58553b6fcbef98f3c8e5253
Beatriz Azevedo <beatriznazevedo@gmail.com>
2022-05-27 01:00:55 -0300

    modulo 3

---
+++ javascript/aula2/script.js
@@ -0,0 +1,108 @@
+/*
+
+// boolean
+var vDuF = false;
+console.log(typeof(vDuF));
+
+// number
+var numeroQualquer =1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'beatriz';
+console.log(typeof(nome));
+
+// como declarar
+var variavel = 'beatriz';
+variavel = 'sidney';
+console.log(variavel);
+
+let variavel2 = 'beatriz';
+variavel2 = 'sidney';
+console.log(variavel2);
+
+*/
+
+/* essa da erro pq não pode mudar o valor da variável const, como é feito na segunta linha com o 'sidney' 
+
+const constante = 'beatriz';
+constante = 'sidney'
+console.log(constante); 
+
+*/
+
+/*
+
+// escopo
+var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+var escopoLocal() {
+    let espocoLocalInterno = 'local';
+    console.log(escopoLocalInterno)
+}
+
+escopoLocal();
+
+// atribuição
+ var atribuicao = 'beatriz';
+
+// comparação
+var comparacao = '0'== 0;
+console.log(comparacao);
+
+// comparação idêntica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);
+
+// adicão
+var adicao = 1  1;
+console.log(adicao);
+
+// subtração
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+// multiplicação
+var adicao = 2 * 3;
+console.log(multiplicacao);
+
+// divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+// divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potencialização
+var potencializacao = 2 ** 10;
+console.log(potencializacao);
+
+// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);
+
+// operadores logicos
+var e = true && true;
+console.log(e);
+
+var ou = false || false;
+console.log(ou)
+
+var nao = !true;
+console.log(nao);
+
+*/
