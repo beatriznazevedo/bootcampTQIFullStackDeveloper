0066dad73752bb3b65538c0db9afb4ff99633d81
Beatriz Azevedo <beatriznazevedo@gmail.com>
2022-07-12 13:07:29 -0300

    counter

--- hooks/src/Components/App/Counter/Counter.js
+++ hooks/src/Components/App/Counter/Counter.js
@@ -1,8 +1,16 @@
 function Counter() {
+    let quantity = 10;
+
+    function upQuantity(){
+        quantity = quantity + 1;
+        document.getElementById("counter-box").innerHTML = quantity;
+        console.log(quantity);
+    }
+
     return (
         <>
-            <h1>1</h1>
-            <button>Aumentar</button>
+            <h1 id="counter-box">{quantity}</h1>
+            <button onClick={upQuantity}>Aumentar</button>
         </>
     )
 }
